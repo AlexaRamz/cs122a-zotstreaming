300150dea9195f83376fb23a5f068609b18f768b
AlexaRamz <ramz.alexa@gmail.com>
2025-04-04 19:21:15 -0700

    Improved command-line argument parsing

--- project.py
+++ project.py
@@ -1,11 +1,14 @@
-import sys
 import os
 import csv
+import argparse
+import inspect
+from typing import get_type_hints, Any
 
 # Using MySQL with Python: https://www.w3schools.com/python/python_mysql_getstarted.asp
 import mysql.connector
 
 db = mysql.connector.connect(
+    host="localhost",
     user="test",
     password="password",
     database="cs122a"
@@ -57,13 +60,12 @@ def import_data(folder_name: str) -> bool:
                     try:
                         cursor.execute(sql_command, row)
                     except mysql.connector.Error as e:
-                        # print(f"Error inserting record {row} into table {table_name}: {e}")
+                        print(f"Error inserting record {row} into table {table_name}: {e}")
                         return False
                 db.commit()
         except FileNotFoundError:
-            # print(f"Error: The file {file_path} was not found.")
+            print(f"Error: The file {file_path} was not found.")
             return False
-
     return True
 
 
@@ -85,7 +87,7 @@ def insert_viewer(uid: int, email: str, nickname: str, street: str, city: str, s
     try:
         cursor.execute(user_statement, user_values)
     except mysql.connector.Error as e:
-        # print(f"Error inserting record into Users: {e}")
+        print(f"Error adding user record: {e}")
         return False
 
     # Inser record into Viewers table
@@ -96,12 +98,11 @@ def insert_viewer(uid: int, email: str, nickname: str, street: str, city: str, s
     viewer_values = (uid, subscription, first_name, last_name)
     try:
         cursor.execute(viewer_statement, viewer_values)
+        db.commit()
+        return True
     except mysql.connector.Error as e:
-        # print(f"Error inserting record into Viewers: {e}")
+        print(f"Error adding viewer record: {e}")
         return False
-
-    db.commit()
-    return True
 
 
 def add_genre(uid: int, genre: str) -> bool:
@@ -121,7 +122,7 @@ def add_genre(uid: int, genre: str) -> bool:
         cursor.execute(get_genres_statement)
         result = cursor.fetchone()
     except mysql.connector.Error as e:
-        # print(f"Error retrieving genres for record with uid {uid}: {e}")
+        print(f"Error retrieving genres for user: {e}")
         return False
     
     genres = genre
@@ -131,7 +132,7 @@ def add_genre(uid: int, genre: str) -> bool:
             genre_list.append(genre)  # Add the specified genre
             genres = ";".join(genre_list)
         else:
-            # print(f"User with uid {uid} already has genre {genre}")
+            print(f"User already has genre {genre}")
             return False
 
     # Update genre list for the user
@@ -143,11 +144,10 @@ def add_genre(uid: int, genre: str) -> bool:
     try:
         cursor.execute(update_genres_statement)
         db.commit()
+        return True
     except mysql.connector.Error as e:
-        # print(f"Error updating genres for record with uid {uid}: {e}")
+        print(f"Error updating genres for user: {e}")
         return False
-
-    return True
 
 
 def delete_viewer(uid: int) -> bool:
@@ -163,38 +163,46 @@ def delete_viewer(uid: int) -> bool:
     try:
         cursor.execute(sql_statement)
         db.commit()
+        return True
     except mysql.connector.Error as e:
-        # print(f"Error removing record with uid {uid} from Viewers: {e}")
+        print(f"Error removing viewer record: {e}")
         return False
-
-    return True
 
 
 def insert_movie(rid: int, website_url: str) -> bool:
     """Insert a new movie in the appropriate table(s). Assume that the corresponding Release record already exists."""
     
 
-    """Checks if the movie rid is already in the Releases table"""
-    releases_statement = f"""SELECT rid FROM Releases WHERE rid = {rid};"""
-
+    # Checks if the movie rid is already in the Releases table
+    releases_statement = f"""
+    SELECT rid
+    FROM Movies
+    WHERE rid = {rid};
+    """
     try:
         cursor.execute(releases_statement)
         result = cursor.fetchone()
     except mysql.connector.Error as e:
+        print(f"Error adding movie record: {e}")
         return False
     
-    """If no movie is found with the same rid, add it to the table"""
+    # If no movie is found with the same rid, add it to the table
     if result is None:
-        movie_sql_statement = f"""INSERT INTO Movies(rid, website_url) VALUES (%s, %s);"""
+        movie_sql_statement = f"""
+        INSERT INTO Movies(rid, website_url)
+        VALUES (%s, %s);"""
         movie_val = (rid, website_url)
 
         try: 
-            cursor.execute(movie_sql_statement, movie_val)
-        
+            cursor.execute(movie_sql_statement, movie_val)      
+            db.commit()
+            return True
         except mysql.connector.Error as e:
+            print(f"Error adding movie record: {e}")
             return False
-    db.commit()
-    return True
+    else:
+        print(f"Movie with rid {rid} already exists")
+        return False
 
 
 def insert_session(sid: int, uid: int, rid: int, ep_num: int, initiate_at: str, leave_at: str, quality: str, device: str) -> bool:
@@ -204,16 +212,18 @@ def insert_session(sid: int, uid: int, rid: int, ep_num: int, initiate_at: str, 
     bool: Whether the action was successful
     """
 
-    """Insert new session into the Sessions Table"""
-    session_statement = f"""INSERT INTO Sessions (sid, uid, rid, ep_num, initiate_at, leave_at, quality, device) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"""
+    session_statement = f"""
+    INSERT INTO Sessions (sid, uid, rid, ep_num, initiate_at, leave_at, quality, device)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+    """
     session_values = (sid, uid, rid, ep_num, initiate_at, leave_at, quality, device)
 
     try:
         cursor.execute(session_statement, session_values)
         db.commit()
         return True
-
     except mysql.connector.Error as e:
+        print(f"Error adding session. Please ensure the viewer, video, and episode number are valid: {e}")
         return False
 
 
@@ -223,19 +233,19 @@ def update_release(rid: int, title: str) -> bool:
     Returns:
     bool: Whether the action was successful
     """
-    """Updates the title on the Release Table"""
-    update_release = f"""UPDATE Releases SET title = %s WHERE rid = %s;"""
-
+    update_release = f"""
+    UPDATE Releases
+    SET title = %s
+    WHERE rid = %s;
+    """
     try:
         cursor.execute(update_release, (title, rid))
         db.commit()
         return True
-    
     except mysql.connector.Error as e:
+        print(f"Error updating release: {e}")
         return False
     
-    """Updates the title in the Videos table??? Not sure if the title is different to the one in Releases"""
-
 
 def list_releases(uid: int) -> None:
     """Given a viewer ID, list all the unique releases the viewer has reviewed in ASCENDING order on the release title
@@ -243,19 +253,22 @@ def list_releases(uid: int) -> None:
     Returns:
     Table - rid, genre, title
     """
-    """Might have to change sql statement below"""
-    get_release_statement = f"""SELECT DISTINCT r.rid, r.genre, r.title FROM Releases as r JOIN Reviews as review ON review.rid = r.rid WHERE review.uid = %s ORDER BY title ASC;"""
+    get_release_statement = f"""
+    SELECT DISTINCT r.rid, r.genre, r.title 
+    FROM Releases as r 
+    JOIN Reviews as review ON review.rid = r.rid 
+    WHERE review.uid = %s 
+    ORDER BY title ASC;"""
     
     try:
         cursor.execute(get_release_statement, (uid, ))
         result = cursor.fetchall()
-        #print("here")
-        """If the output is a result table, print each record in one line and separate columns with ‘,’ - just like the format of the dataset file. """
+
         for i in result:
             print(f"{i[0]},{i[1]},{i[2]}")
     
     except mysql.connector.Error as e:
-        pass
+        print(f"Error retrieving releases for viewer: {e}")
 
 
 def list_popular_releases(N: int):
@@ -265,15 +278,15 @@ def list_popular_releases(N: int):
     Table - rid, title, reviewCount
     """
     get_popular_releases = f"""
-        WITH RevCounts AS (
-            SELECT rid, COUNT(*) as reviewCount
-            FROM Reviews
-            GROUP BY rid)
-        SELECT r.rid, r.title, COALESCE(rc.reviewCount, 0) as reviewCount
-        FROM Releases r LEFT JOIN RevCounts rc ON r.rid = rc.rid
-        ORDER BY reviewCount DESC, rid DESC
-        LIMIT {N}
-        """
+    WITH RevCounts AS (
+        SELECT rid, COUNT(*) as reviewCount
+        FROM Reviews
+        GROUP BY rid)
+    SELECT r.rid, r.title, COALESCE(rc.reviewCount, 0) as reviewCount
+    FROM Releases r LEFT JOIN RevCounts rc ON r.rid = rc.rid
+    ORDER BY reviewCount DESC, rid DESC
+    LIMIT {N}
+    """
     
     try:
         cursor.execute(get_popular_releases)
@@ -283,7 +296,7 @@ def list_popular_releases(N: int):
             print(f"{i[0]},{i[1]},{i[2]}")
     
     except mysql.connector.Error as e:
-        print(f"Error: {e}")
+        print(f"Error retrieving releases: {e}")
 
 
 def get_release_title(sid: int):
@@ -293,19 +306,19 @@ def get_release_title(sid: int):
     Table - rid, release_title, genre, video_title, ep_num, length
     """
     get_release_info = f"""
-        WITH RelVids AS (SELECT r.rid,
-                                r.title AS release_title,
-                                r.genre,
-                                v.ep_num,
-                                v.title AS video_title,
-                                v.length
-              FROM Releases r
-              JOIN Videos v ON r.rid = v.rid)
-        SELECT rid, release_title, genre, video_title, ep_num, length
-        FROM Sessions NATURAL JOIN RelVids
-        WHERE sid = {sid}
-        ORDER BY release_title ASC
-        """
+    WITH RelVids AS (SELECT r.rid,
+                            r.title AS release_title,
+                            r.genre,
+                            v.ep_num,
+                            v.title AS video_title,
+                            v.length
+            FROM Releases r
+            JOIN Videos v ON r.rid = v.rid)
+    SELECT rid, release_title, genre, video_title, ep_num, length
+    FROM Sessions NATURAL JOIN RelVids
+    WHERE sid = {sid}
+    ORDER BY release_title ASC
+    """
         
     try:
         cursor.execute(get_release_info)
@@ -315,7 +328,7 @@ def get_release_title(sid: int):
             print(f"{i[0]},{i[1]},{i[2]},{i[3]},{i[4]},{i[5]}")
     
     except mysql.connector.Error as e:
-        print(f"Error: {e}")
+        print(f"Error retrieving release for session: {e}")
 
 
 def list_active_viewers(N: int, start: str, end: str):
@@ -325,16 +338,16 @@ def list_active_viewers(N: int, start: str, end: str):
     Table - UID, first name, last name
     """
     get_active_viewers = f"""
-        WITH AppSessions AS (
-              SELECT uid, COUNT(*) as userCount
-              FROM Sessions
-              WHERE initiate_at >= '{start}' AND leave_at <= '{end}'
-              GROUP BY uid)
-        SELECT uid, first_name, last_name
-        FROM Viewers NATURAL JOIN AppSessions
-        WHERE userCount >= {N}
-        ORDER BY uid ASC
-        """
+    WITH AppSessions AS (
+            SELECT uid, COUNT(*) as userCount
+            FROM Sessions
+            WHERE initiate_at >= '{start}' AND leave_at <= '{end}'
+            GROUP BY uid)
+    SELECT uid, first_name, last_name
+    FROM Viewers NATURAL JOIN AppSessions
+    WHERE userCount >= {N}
+    ORDER BY uid ASC
+    """
         
     try:
         cursor.execute(get_active_viewers)
@@ -344,26 +357,26 @@ def list_active_viewers(N: int, start: str, end: str):
             print(f"{i[0]},{i[1]},{i[2]}")
     
     except mysql.connector.Error as e:
-        print(f"Error: {e}")
+        print(f"Error retrieving viewers: {e}")
 
 
 def get_videos_viewed(rid: int):
     """Given a Video rid, count the number of unique viewers that have started a session on it. Videos that are not streamed by any viewer should have a count of 0 instead of NULL. Return video information along with the count in DESCENDING order by rid.
 
     Returns:
-    Table - RID, ep_num, title, length,COUNT
+    Table - RID, ep_num, title, length, COUNT
     """
     get_video_views = f"""
-        WITH UniqueUserCount AS (
-              SELECT rid, COUNT(DISTINCT uid) as user_count
-              FROM Sessions
-              GROUP BY rid)
-        SELECT rid, ep_num, title, length, COALESCE(user_count, 0)
-        FROM Videos
-        LEFT JOIN UniqueUserCount USING (rid)
-        WHERE rid = {rid}
-        ORDER BY rid DESC
-        """
+    WITH UniqueUserCount AS (
+            SELECT rid, COUNT(DISTINCT uid) as user_count
+            FROM Sessions
+            GROUP BY rid)
+    SELECT rid, ep_num, title, length, COALESCE(user_count, 0)
+    FROM Videos
+    LEFT JOIN UniqueUserCount USING (rid)
+    WHERE rid = {rid}
+    ORDER BY rid DESC
+    """
         
     try:
         cursor.execute(get_video_views)
@@ -373,78 +386,55 @@ def get_videos_viewed(rid: int):
             print(f"{i[0]},{i[1]},{i[2]},{i[3]},{i[4]}")
     
     except mysql.connector.Error as e:
-        print(f"Error: {e}")
+        print(f"Error retrieving videos for release: {e}")
 
+def add_function_arguments(parser: argparse.ArgumentParser, func: Any):
+    """Adds arguments to the parser based on the function's signature."""
+    # Easily create CLI to run Python functions: https://medium.com/@iliazaitsev/run-a-python-function-from-terminal-1fc773e08b44
+    signature = inspect.signature(func)
+    type_hints = get_type_hints(func)
+
+    for name in signature.parameters.keys():
+        arg_type = type_hints.get(name, str) # Default to str if no type hint
+        parser.add_argument(name, type=arg_type)
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        cmd = sys.argv[1]
-        args = []
-
-        # Get only the arguments for the function (exclude script name and function name)
-        if len(sys.argv) > 2:
-            args = sys.argv[2:] 
+        # Create a command for each function of the form <function name> [param1] [param2] ...
+        functions = {
+            "import": import_data,
+            "insertViewer": insert_viewer,
+            "addGenre": add_genre,
+            "deleteViewer": delete_viewer,
+            "insertMovie": insert_movie,
+            "insertSession": insert_session,
+            "updateRelease": update_release,
+            "listReleases": list_releases,
+            "popularRelease": list_popular_releases,
+            "releaseTitle": get_release_title,
+            "activeViewer": list_active_viewers,
+            "videosViewed": get_videos_viewed,
+        }
+        parser = argparse.ArgumentParser()
+        subparsers = parser.add_subparsers(dest="function")
         
-        # Process arguments
-        for i in range(len(args)):
-            args[i] = args[i].strip("\"") # Remove quotations for strings that may have spaces
-            if args[i] == "NULL":
-                args[i] = None # Replace "NULL" with None
+        for name, func in functions.items():
+            subparser = subparsers.add_parser(name, help=func.__doc__)
+            add_function_arguments(subparser, func)
+
+        # Parse command-line arguments
+        args = parser.parse_args()
+
+        if args.function:
+            func = functions[args.function]
+            func_args = {k: v for k, v in vars(args).items() if k != "function"} # Get only the arguments for the function (exclude function name)
+
+            for key in func_args:
+                if type(func_args[key]) is str: # Check value is a string
+                    func_args[key] = func_args[key].strip("\"") # Remove quotations for strings that may have spaces
+                    if func_args[key] == "NULL":
+                        func_args[key] = None # Replace "NULL" with None
+                    
+            result = func(**func_args) # Call the function with parsed arguments
             
-        result = None
-        try:
-            if cmd == "import":
-                result = import_data(*args)
-            elif cmd == "insertViewer":
-                args[0] = int(args[0])
-                result = insert_viewer(*args)
-            elif cmd == "addGenre":
-                args[0] = int(args[0])
-                result = add_genre(*args)
-            elif cmd == "deleteViewer":
-                args[0] = int(args[0])
-                result = delete_viewer(*args)
-            elif cmd == "insertMovie":
-                args[0] = int(args[0])
-                result = insert_movie(*args)
-            elif cmd == "insertSession":
-                args[0] = int(args[0])
-                args[1] = int(args[1])
-                args[2] = int(args[2])
-                args[3] = int(args[3])
-
-                result = insert_session(*args)
-            elif cmd == "updateRelease":
-                args[0] = int(args[0])
-                result = update_release(*args)
-            elif cmd == "listReleases":
-                args[0] = int(args[0])
-                result = list_releases(*args)
-            elif cmd == "popularRelease":
-                args[0] = int(args[0])
-                result = list_popular_releases(*args)
-            elif cmd == "releaseTitle":
-                args[0] = int(args[0])
-                result = get_release_title(*args)
-            elif cmd == "activeViewer":
-                args[0] = int(args[0])
-                result = list_active_viewers(*args)
-            elif cmd == "videosViewed":
-                args[0] = int(args[0])
-                result = get_videos_viewed(*args)
-            else:
-                print("Command not found")
-
             if type(result) is bool:
-                if result:
-                    print("Success")
-                else:
-                    print("Fail")
-
-        except ValueError as e:
-            print(f"Wrong type for one or more arguments: ", {e})
-        except TypeError as e:
-            print(
-                f"Invalid number of arguments, or wrong type for one or more arguments: ", {e})
-        except IndexError as e:
-            print(f"Invalid number of arguments: ", {e})
+                print("Success" if result else "Fail")
